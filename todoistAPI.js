3570891a18907ee540a8bca0b8f4c1e61b969809
Micael Levi L. Cavalcante <mllc@icomp.ufam.edu.br>
2019-03-02 23:02:56 -0400

    up: interface de acesso à API mais facilitada e ortogonal

---
+++ todoistAPI.js
@@ -0,0 +1,78 @@
+// Todoist's API Reference: https://developer.todoist.com/sync/v7
+const axios = require('axios');
+
+const REGEX_ITEM_CONTENT = /^(?:\{([^}]+)\}\s*)?([^(\s]+)(?:\s*\((.+)\))?/;
+const REGEX_PLAYLIST_CONTENT = /\bplaylist\b/;
+
+
+function formatDate(dateAdded) {
+  //return dateAdded.replace(/\w+\s(\d+)\s(\w+)\s(\d+).*/, '$2 $1, $3');
+  const [, day, month, year] = dateAdded.split(' ', 4);
+  return `${month} ${day}, ${year}`;
+}
+
+function formatContent(content) {
+  const matches = content.match(REGEX_ITEM_CONTENT);
+  if (!matches) return content;
+
+  const contentMetadata = {
+    link: matches[2],
+    text: matches[3],
+  };
+
+  const tag = matches[1]
+            ? matches[1].toLowerCase()
+            : ( REGEX_PLAYLIST_CONTENT.test(content) && 'playlist' );
+
+  return tag
+       ? { ...contentMetadata, tag }
+       : contentMetadata;
+}
+
+function formatProjectItems({ items }, minIndent = 2) {
+  const formatItem = item => ({
+    indent: item.indent,
+    checked: item.checked,
+    dateAdded: formatDate(item.date_added),
+    id: item.id,
+    priority: item.priority,
+    content: formatContent(item.content),
+  });
+
+  let selectedItems = [];
+  let excludedItemsId = [];
+
+  for (const item of items) {
+    if (item.content.endsWith('✖️')) { // item onde os 'filhos' também devem ser excluídos
+      excludedItemsId.push(item['id']);
+    } else {
+      if (item.indent < minIndent &&
+          !item.content.startsWith('~ ')) continue; // item que não deve ser listado
+
+      if (item['parent_id'] &&
+          !excludedItemsId.includes(item['parent_id'])) selectedItems.push( formatItem(item) );
+    }
+  }
+
+  return selectedItems;
+}
+
+function makeInstance(apiToken) {
+  const conn = axios.create({
+    baseURL: 'https://todoist.com/api/v7',
+    headers: {
+      'Authorization': `Bearer ${apiToken}`
+    }
+  });
+
+  const getProjectItems = projectId =>
+    conn.post('projects/get_data', { project_id: projectId })
+        .then(({ data }) => formatProjectItems(data))
+
+  return {
+    getProjectItems,
+  }
+}
+
+
+module.exports = makeInstance
